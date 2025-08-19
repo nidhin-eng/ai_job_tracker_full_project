ab27bd0b5a3ce5f0fa05cf2b6f7865e0cf4d8090
nidhin-eng <nidhinudupa@gmail.com>
2025-08-19 22:24:24 +0530

    Fix: added SSL support in index.js for Render PostgreSQL

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render PostgreSQL
+  },
 });
 
 module.exports = pool;
-
