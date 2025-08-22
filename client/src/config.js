6315c7b7d443ad7999cb89a29b081f4bd134a952
nidhin-eng <nidhinudupa@gmail.com>
2025-08-22 20:49:19 +0530

    Update config for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "https://ai-job-tracker-full-project-1.onrender.com";
+const API_BASE_URL = "https://ai-job-tracker-full-project-1.onrender.com/api";
 ;
 export default API_BASE_URL;
